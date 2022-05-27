642c5959b58bfa0391434d84d521882e891c4c5f
Iliukuhin Ilia <iiliukhin@gmail.com>
2022-05-27 19:23:01 +0300

    add allow hosts

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,15 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_KEY', default='qwerty')
 DEBUG = False
-ALLOWED_HOSTS = os.getenv('HOSTS', default='localhost')
+# ALLOWED_HOSTS = os.getenv('HOSTS', default='localhost')
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+    '51.250.102.12'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
