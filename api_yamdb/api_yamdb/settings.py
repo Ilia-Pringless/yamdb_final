7e4e67bc242dbd002665a356a8e74cdfa087658c
Iliukuhin Ilia <iiliukhin@gmail.com>
2022-05-29 19:04:48 +0300

    debug false

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_KEY', default='qwerty')
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = os.getenv('HOSTS', default='localhost')
 
 ALLOWED_HOSTS = [
